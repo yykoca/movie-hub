d72887fb65396fa09f479e8876f1fdbb3af2860c
yykoca <yykoca.08@gmail.com>
2021-07-27 17:14:40 +0200

    new Pages added

--- src/Pages/Header.js
+++ src/Pages/Header.js
@@ -1,21 +1,22 @@
 import React from "react";
 import { Navbar, Nav, Form, Button, FormControl } from "react-bootstrap";
+import { BiCameraMovie } from 'react-icons/bi';
 
 function Header() {
   return (
     <Navbar collapseOnSelect bg="dark" expand="lg" variant="dark">
-      <Navbar.Brand href="#">Movie-Hub</Navbar.Brand>
+      <Navbar.Brand href="/"><BiCameraMovie /> Movie-Hub</Navbar.Brand>
       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
       <Navbar.Collapse id="responsive-navbar-nav">
         <Nav
           className="mr-auto my-2 my-lg-0 justify-content-end"
           style={{ maxHeight: "100px" }}>
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#favorites">Favorites</Nav.Link>
-          <Nav.Link href="#watched">Watched Films</Nav.Link>
-          <Nav.Link href="#about">About</Nav.Link>
+          <Nav.Link href="/">Home</Nav.Link>
+          <Nav.Link href="/favorites">Favorites</Nav.Link>
+          <Nav.Link href="/watched">Watched Films</Nav.Link>
+          <Nav.Link href="/about">About</Nav.Link>
         </Nav>
-        {/* <Form className="d-flex">
+        <Form className="d-flex">
           <FormControl
             type="search"
             placeholder="Search"
@@ -23,7 +24,7 @@ function Header() {
             aria-label="Search"
           />
           <Button variant="outline-success">Search</Button>
-        </Form> */}
+        </Form>
       </Navbar.Collapse>
     </Navbar>
   );
