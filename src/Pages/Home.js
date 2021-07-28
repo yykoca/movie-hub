54f9de6f40442a5ddef70c22ae104672b91cc72a
yykoca <yykoca.08@gmail.com>
2021-07-28 19:02:38 +0200

    responsive view set

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,11 +1,37 @@
-import React from 'react'
+import React from "react";
+import { Container, Row } from "react-bootstrap";
+import Cards from "../Components/Card";
 
-function Home() {
-    return (
-        <div>
-            <h1>Home</h1>
-        </div>
-    )
+function Home({ movies, pageNumber, setPageNumber }) {
+  console.log("ne geldi home", movies);
+  const moviesArray = [];
+  for (let movie in movies) {
+    moviesArray.push(movies[movie]);
+  }
+
+  function makePagePrevious() {
+    if (pageNumber !== 1) {
+      setPageNumber(pageNumber - 1);
+    }
+  }
+
+  function makePageNext() {
+    setPageNumber(pageNumber + 1);
+  }
+  return (
+    <Container fluid="md">
+      <Row xs={1} sm={2} md={3} lg={4} className="g-4">
+        {moviesArray.map((movie, key) => {
+          return <Cards movie={movie} key={key} />;
+        })}
+      </Row>
+      <br />
+      {pageNumber} <br />
+      <br />
+      <button onClick={makePagePrevious}>Previous</button>
+      <button onClick={makePageNext}>Next</button>
+    </Container>
+  );
 }
 
-export default Home
+export default Home;
